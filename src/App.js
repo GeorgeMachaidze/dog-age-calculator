5cfe765ae352a216da84cb8e642afa5a0ea9e7d8
Wakana <wakaichi@gmail.com>
2023-03-24 10:57:36 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChangeEvent, useState } from "react";
+
+
+function calculateDogAge(dogAge) {
+  
+  if (dogAge === 1) {
+    return 15;
+  } else if (dogAge === 2) {
+    
+    return 15 + 9;
+  } else {
+    
+    return 15 + 9 + ((dogAge - 2) * 5);
+  }
+}
 
 function App() {
+  const [dogAge, setDogAge] = useState("");
+  const [humanAge, setHumanAge] = useState("");
+
+  const handleInputChange = (event) => {
+    setDogAge(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (!isNaN(dogAge) && Number(dogAge) > 0) {
+      const humanAge = calculateDogAge(Number(dogAge));
+      setHumanAge(humanAge);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='header-text'>Calculate Your Dog's Age In Human Years</h1>
+        <div className='main-container'>
+        <form>
+          <label>
+            <p>Enter your dog age</p>
+            <input type="number" value={dogAge} onChange={handleInputChange} />
+
+          </label>
+          <button onClick={handleClick}>Calculate</button>
+        </form>
+
+        {humanAge !== "" &&(
+          <div>
+          <p>Your dog's age in human years is {humanAge}</p>
+        </div>
+        )}
+        </div>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
